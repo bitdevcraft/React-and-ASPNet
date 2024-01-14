abec813845c34ff3e3d0865f25f42d7a18d9d152
bitdevcraft <ryanvincent.capio@outlook.com>
2024-01-14 15:57:19 +0400

    Uses Mobx and refactor the code

--- client_app/src/features/activities/dashboard/ActivityList.tsx
+++ client_app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,13 @@
-import React, { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props {
-  activities: Activity[];
-  selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
-
-export default function ActivityList({
-  activities,
-  selectActivity,
-  deleteActivity,
-  submitting,
-}: Props) {
+export default observer(function ActivityList() {
   const [target, setTarget] = useState('');
+
+  const { activityStore } = useStore();
+  const { deleteActivity, loading, activitiesByDate } = activityStore;
 
   function handleActivityDelete(
     e: SyntheticEvent<HTMLButtonElement>,
@@ -28,7 +20,7 @@ export default function ActivityList({
   return (
     <Segment>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as="a">{activity.title}</Item.Header>
@@ -44,11 +36,11 @@ export default function ActivityList({
                   floated="right"
                   content="View"
                   color="blue"
-                  onClick={() => selectActivity(activity.id)}
+                  onClick={() => activityStore.selectActivity(activity.id)}
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   floated="right"
                   content="Delete"
                   color="red"
@@ -62,4 +54,4 @@ export default function ActivityList({
       </Item.Group>
     </Segment>
   );
-}
+});
--- client_app/src/features/activities/details/ActivityDetails.tsx
+++ client_app/src/features/activities/details/ActivityDetails.tsx
@@ -1,32 +1,38 @@
-import React from 'react';
-import {Activity} from "../../../app/models/activity";
-import {Button, Card, Image} from "semantic-ui-react";
+import { Button, Card, Image } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activity: Activity
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default function ActivityDetails() {
+  const { activityStore } = useStore();
+  const { selectedActivity: activity } = activityStore;
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
-    return (
-        <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-            <Card.Content>
-                <Card.Header>{activity.title}</Card.Header>
-                <Card.Meta>
-                    <span>{activity.date}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {activity.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
-                </Button.Group>
-            </Card.Content>
-        </Card>
-    )
+  if (!activity) return <></>;
+
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths="2">
+          <Button
+            onClick={() => activityStore.openForm(activity.id)}
+            basic
+            color="blue"
+            content="Edit"
+          />
+          <Button
+            onClick={activityStore.cancelSelectedActivity}
+            basic
+            color="grey"
+            content="Cancel"
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
 }
